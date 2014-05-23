f8a98ede0dc1684ab5a273884d991334c8b19071
Joe Andaverde <joe@andaverde.net>
2014-05-22 23:27:02 -0500

    Regex choice and concat

--- nfa.js
+++ nfa.js
@@ -16,19 +16,38 @@ var NFARulebook = function (rules) {
    this.rules = rules;
 };
 
+NFARulebook.prototype.follow_free_moves = function (states) {
+
+   var more_states = this.next_states(states, null);
+
+   if (more_states.length === 0) return states;
+
+   var new_states = more_states
+   .filter(function (x) {
+      return states.indexOf(x) < 0;
+   });
+
+   if (new_states.length > 0) {
+      return this.follow_free_moves(states.concat(new_states));
+   }
+
+   return states;
+};
+
 NFARulebook.prototype.next_states = function (states, character) {
-   var results = {};
+   var results = [];
 
    for (var i = 0; i < states.length; i++) {
       var state = states[i];
       this.follow_rules_for(state, character)
       .forEach(function (s) {
-         results[s] = true;
+         if (results.indexOf(s) < 0) {
+            results.push(s);
+         }
       });
    }
-   return Object.keys(results).map(function (x) {
-      return parseInt(x, 10);
-   });
+
+   return results;
 };
 
 NFARulebook.prototype.follow_rules_for = function (state, character) {
@@ -60,6 +79,8 @@ var rulebook = new NFARulebook([
 ]);
 
 var NFA = function (current_states, accept_states, rulebook) {
+   current_states = [].concat(current_states);
+
    this.read_string = function (s) {
       for (var i = 0; i < s.length; i++) {
          this.read_character(s[i]);
@@ -67,12 +88,17 @@ var NFA = function (current_states, accept_states, rulebook) {
    };
 
    this.read_character = function (c) {
-      current_states = rulebook.next_states(current_states, c);
+      current_states = rulebook.next_states(this.get_current_states(), c);
+   };
+
+   this.get_current_states = function () {
+      return rulebook.follow_free_moves(current_states);
    };
 
    this.accepting = function () {
-      for (var i = 0; i < current_states.length; i++) {
-         if (accept_states.indexOf(current_states[i]) >= 0) {
+      var states = this.get_current_states();
+      for (var i = 0; i < states.length; i++) {
+         if (accept_states.indexOf(states[i]) >= 0) {
             return true;
          }
       }
@@ -81,6 +107,10 @@ var NFA = function (current_states, accept_states, rulebook) {
 };
 
 var NFADesign = function (current, accept, rulebook) {
+   this.start_state = current;
+   this.accept_states = accept;
+   this.rulebook = rulebook;
+
    this.accepts = function (s) {
       var nfa = new NFA(current, accept, rulebook);
       nfa.read_string(s);
@@ -88,6 +118,31 @@ var NFADesign = function (current, accept, rulebook) {
    };
 };
 
-var x = new NFADesign([1], [4], rulebook);
+var x = new NFADesign(1, [4], rulebook);
 console.log(x.accepts('bbbbb')); //true
 console.log(x.accepts('bbabb')); //false
+
+// Free moves
+var rulebook_free = new NFARulebook([
+   new FARule(1, null, 2),
+   new FARule(1, null, 4),
+   new FARule(2, 'a', 3),
+   new FARule(3, 'a', 2),
+   new FARule(4, 'a', 5),
+   new FARule(5, 'a', 6),
+   new FARule(6, 'a', 4)
+]);
+
+console.log(rulebook_free.follow_free_moves([1]));
+
+var design_2 = new NFADesign(1, [2, 4], rulebook_free);
+
+console.log(design_2.accepts('aa')); //true
+console.log(design_2.accepts('aaa')); //true
+console.log(design_2.accepts('aaaaa')); //false
+
+module.exports = {
+   NFADesign: NFADesign,
+   NFARulebook: NFARulebook,
+   FARule: FARule
+};
--- regex.js
+++ regex.js
@@ -1,3 +1,8 @@
+var nfa = require('./nfa.js');
+var NFADesign = nfa.NFADesign;
+var NFARulebook = nfa.NFARulebook;
+var FARule = nfa.FARule;
+
 var extend = function (child, parent) {
    child.prototype = Object.create(parent.prototype);
    child.prototype.constructor = child;
@@ -16,9 +21,18 @@ Pattern.prototype.toString = function () {
    return "/" + this.value + "/";
 };
 
+Pattern.prototype.matches = function (str) {
+   return this.to_nfa_design().accepts(str);
+};
+
 var Empty = function () {
    this.value = '';
    this.precedence = 3;
+   this.to_nfa_design = function () {
+      var rulebook = new NFARulebook([]);
+      var start_state = {};
+      return new NFADesign(start_state, [start_state], rulebook);
+   };
 };
 
 extend(Empty, Pattern);
@@ -26,6 +40,13 @@ extend(Empty, Pattern);
 var Literal = function (c) {
    this.value = c;
    this.precedence = 3;
+   this.to_nfa_design = function () {
+      var start_state = {};
+      var accept_state = {};
+      var rule = new FARule(start_state, c, accept_state);
+      var rulebook = new NFARulebook([rule]);
+      return new NFADesign(start_state, [accept_state], rulebook);
+   };
 };
 
 extend(Literal, Pattern);
@@ -33,6 +54,23 @@ extend(Literal, Pattern);
 var Concatenate = function (first, second) {
    this.precedence = 1;
    this.value = first.bracket(this.precedence) + second.bracket(this.precedence);
+   this.to_nfa_design = function () {
+      var first_nfa = first.to_nfa_design();
+      var second_nfa = second.to_nfa_design();
+
+      var start_state = first_nfa.start_state;
+      var accept_states = second_nfa.accept_states;
+
+      var new_rules = first_nfa.rulebook.rules.concat(second_nfa.rulebook.rules);
+
+      var extra_rules = first_nfa.accept_states.map(function (s) {
+         return new FARule(s, null, second_nfa.start_state);
+      });
+
+      var rules = new_rules.concat(extra_rules);
+
+      return new NFADesign(start_state, accept_states, new NFARulebook(rules));
+   };
 };
 
 extend(Concatenate, Pattern);
@@ -40,6 +78,24 @@ extend(Concatenate, Pattern);
 var Choose = function (first, second) {
    this.precedence = 0;
    this.value = first.bracket(this.precedence) + '|' + second.bracket(this.precedence);
+
+   this.to_nfa_design = function () {
+      var first_nfa = first.to_nfa_design();
+      var second_nfa = second.to_nfa_design();
+
+      var start_state = {};
+
+      var accept_states = second_nfa.accept_states.concat(first_nfa.accept_states);
+      var new_rules = first_nfa.rulebook.rules.concat(second_nfa.rulebook.rules);
+
+      var extra_rules = [first_nfa, second_nfa].map(function (nfa) {
+         return new FARule(start_state, null, nfa.start_state);
+      });
+
+      var rules = new_rules.concat(extra_rules);
+
+      return new NFADesign(start_state, accept_states, new NFARulebook(rules));
+   };
 };
 
 extend(Choose, Pattern);
@@ -58,4 +114,38 @@ var p = new Repeat(
    )
 );
 
-console.log(p.toString())
+console.log(p.toString());
+
+//Regex
+console.log('regex nfa');
+console.log(new Array(10).join('='));
+var design = new Empty().to_nfa_design();
+
+console.log(design.accepts('')); // true
+console.log(design.accepts('a')); // false
+
+design = new Literal('a').to_nfa_design();
+
+console.log(design.accepts('')); // false
+console.log(design.accepts('a')); // true
+
+var pattern = new Concatenate(
+   new Literal('a'),
+   new Concatenate(new Literal('b'), new Literal('c'))
+);
+
+console.log('Joined Regex NFAs');
+console.log(pattern.toString());
+console.log(new Array(10).join('='));
+console.log(pattern.matches('a')); //false
+console.log(pattern.matches('ab')); //false
+console.log(pattern.matches('abc')); //true
+
+pattern = new Choose(
+   new Literal('a'),
+   new Literal('b')
+);
+console.log(pattern.toString());
+console.log('a', '->', pattern.matches('a')); // true
+console.log('b', '->', pattern.matches('b')); // true
+console.log('c', '->', pattern.matches('c')); // false
